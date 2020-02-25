8bf9a000bc3118c1baaaea68f3d8d50d2641ee23
PRoman93 <roman_prokopets@mail.ua>
2020-02-25 14:39:03 +0200

    Updates

---
+++ precache-manifest.03c5128994b912b6b0aae0e99eaec70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a64fd0829d97574803015971b1b589",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "f297d9414d5c3d5c8b22",
+    "url": "/counter/static/css/main.f86c4059.chunk.css"
+  },
+  {
+    "revision": "74b5be99e6feff1d49b5",
+    "url": "/counter/static/js/2.515fe849.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter/static/js/2.515fe849.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f297d9414d5c3d5c8b22",
+    "url": "/counter/static/js/main.2a7119f0.chunk.js"
+  },
+  {
+    "revision": "09b1d7882e9d8bcfac4f",
+    "url": "/counter/static/js/runtime-main.15d4b3e4.js"
+  }
+]);
